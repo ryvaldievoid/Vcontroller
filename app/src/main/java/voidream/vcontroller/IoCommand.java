902ad5c5758b4c7378a8eb03a9ba133acf89d148
ryvaldie <ryvaldiehamdallah@gmail.com>
2016-07-18 21:20:28 +0700

    setonitemclicklistener

--- app/src/main/java/voidream/vcontroller/IoCommand.java
+++ app/src/main/java/voidream/vcontroller/IoCommand.java
@@ -1,7 +1,10 @@
 package voidream.vcontroller;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class IoCommand extends Activity {
@@ -16,6 +19,13 @@ public class IoCommand extends Activity {
         AdapterListIoCommand adapterListIoCommand = new AdapterListIoCommand(this);
         list_io_command = (ListView)findViewById(R.id.listview_io_command_list_output);
         list_io_command.setAdapter(adapterListIoCommand);
+        list_io_command.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent io_form = new Intent(IoCommand.this, IoCommandForm.class);
+                startActivity(io_form);
+            }
+        });
 
     }
 }
